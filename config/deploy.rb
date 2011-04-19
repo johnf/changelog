867d1837630941c9ed800ef009314638d9d416cf
John Ferlito <johnf@inodes.org>
2011-04-20 00:37:58 +1000

    capistrano setup

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+require "bundler/capistrano"
+
+set :application, 'changelog'
+set :repository,  'git@github.com:johnf/changelog.git'
+
+set :scm, :git
+set :deploy_via, :remote_cache
+
+set :user, 'deploy'
+set :use_sudo, false
+
+set :deploy_to, '/srv/www/rubychangelog.org'
+
+
+role :web, 'beaker.inodes.org'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
