b046ed3374e067c3693e011c517c5c642fe8497e
John Ferlito <johnf@inodes.org>
2011-04-23 13:27:56 +1000

    Add admin_signed_in? as a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  helper_method :admin_signed_in?
+
   private
   def admin_signed_in?
     user_signed_in? and current_user.admin?
